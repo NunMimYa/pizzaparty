84ce78d82d1451f35c31727fdd7997706b9d554b
NunMimYa <ljezequelpfl@hotmail.fr>
2020-12-14 16:17:11 +0100

    commit #2 click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,4 +18,20 @@ export class AppComponent {
     image: '4-fromages.jpg'
   };
 
+  mesPizzas: Pizza[] = [
+    {id:1, name:'Reine', price: 12, image:'reine.jpg'},
+    {id:2, name:'4 fromages', price: 13, image:'4-fromages.jpg'},
+    {id:3, name:'Orientale', price: 11, image:'orientale.jpg'},
+    {id:4, name:'Cannibale', price: 9, image:'cannibale.jpg'},
+  ];
+
+  // On peut créer une fonction / une méthode
+  onSelect(pizza: Pizza){
+    // console.log('ça marche');
+    // console.log(this);
+    // console.log(pizza.name + ' ' + pizza.price + ' $');
+    this.maSuperPizza = pizza;
+    // console.log(this.mesPizzas.name);
+  }
+
 }
