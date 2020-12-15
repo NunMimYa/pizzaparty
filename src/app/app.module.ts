f9e48c91e57002a6902d679d828c822c50cb5573
NunMimYa <ljezequelpfl@hotmail.fr>
2020-12-15 14:50:52 +0100

    commit #8 generate component home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,25 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { PizzaListComponent } from './pizza-list/pizza-list.component';
 import { MenuComponent } from './menu/menu.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PizzaListComponent,
-    MenuComponent, 
+    MenuComponent,
+    HomeComponent, 
     
   ],
   imports: [
     BrowserModule,
     FormsModule, 
-    NgbModule
+    NgbModule, 
+    RouterModule.forRoot([
+      {path: '', component: PizzaListComponent },
+      {path: 'pizzas', component: PizzaListComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
